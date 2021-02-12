a1dc8b8d19ad1ebef2abc8f15b1c106e9c6f9982
ArtDor1978 <artdor1978@gmail.com>
2021-02-12 11:53:52 +0200

    timer

--- src/app.js
+++ src/app.js
@@ -5,16 +5,39 @@ import Init from "./init.js";
 const App = () => {
 	const [work, setWork] = useState(25);
 	const [relax, setRelax] = useState(5);
+	const [timeLeft, setTime] = useState(work + ":00");
+	let seconds = 60;
+	const y = work - 1;
+
 	return (
 		<>
 			<div className="init">
 				<Init setCount={setWork} count={work} label="session" />
 				<Init setCount={setRelax} count={relax} label="break" />
 			</div>
-			<div id="timer-label">"Session"</div>
-			<div id="time-left">{work}</div>
-			<button id="start_stop">start_stop</button>
-			<button id="reset">reset</button>
+			<div className="process">
+				<div id="timer-label">Session</div>
+				<div id="time-left">{timeLeft}</div>
+				<button
+					id="start_stop"
+					onClick={(function timerCountDown() {
+						console.log("ddd", seconds);
+						if (seconds-- > 0) setTimeout(timerCountDown, 1000);
+					})()}
+				>
+					start_stop
+				</button>
+				<button
+					id="reset"
+					onClick={() => {
+						setWork(25);
+						setRelax(5);
+					}}
+				>
+					reset
+				</button>
+				<div></div>
+			</div>
 		</>
 	);
 };
