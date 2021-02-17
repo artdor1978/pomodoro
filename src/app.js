29afcb0180f726ee44e9944acf359325820a7fc7
ArtDor1978 <artdor1978@gmail.com>
2021-02-17 15:21:23 +0200

    timer done!

--- src/app.js
+++ src/app.js
@@ -1,76 +1,52 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 import "./app.css";
 import Init from "./init.js";
-//import { countDownMinutes } from "./coundown.js";
 function App() {
 	const [work, setWork] = useState(25);
 	const [relax, setRelax] = useState(5);
+	const [seconds, setSeconds] = useState(work * 60);
+	const [isActive, setIsActive] = useState(false);
 	const [timeLeft, setTime] = useState(work + ":00");
-	const [textButton, setText] = useState("Start");
-	let [count2, setCount2] = useState(60);
+
 	useEffect(() => {
 		setTime(work + ":00");
+		setSeconds(work * 60);
 	}, [work]);
 
-	function useInterval(callback, delay) {
-		const savedCallback = useRef();
-
-		// Remember the latest function.
-		useEffect(() => {
-			savedCallback.current = callback;
-		}, [callback]);
-
-		// Set up the interval.
-		useEffect(() => {
-			function tick() {
-				savedCallback.current();
-			}
-			if (delay !== null) {
-				let id = setInterval(tick, delay);
-				return () => clearInterval(id);
-			}
-		}, [delay]);
+	function toggle() {
+		setIsActive(!isActive);
 	}
-	function Counter() {
-		useInterval(() => {
-			// Your custom logic here
-			setCount2(count2 - 1);
-			console.log(count2);
-		}, 1000);
 
-		/*return count2;*/
+	function reset() {
+		setSeconds(work * 60);
+		setIsActive(false);
+		setWork(25);
+		setRelax(5);
+		setTime(work + ":00");
 	}
-	const ssss = Counter();
-	/*var timeoutHandle;
-	console.log("0", timeoutHandle);
-	const countDownMinutes = (minutes) => {
-		let timeString;
-		let seconds = 60;
-		const tick = () => {
-			let currentMinutes = minutes - 1;
-			seconds--;
-			timeString =
-				(currentMinutes < 10 ? "0" : "") +
-				String(currentMinutes) +
-				":" +
-				(seconds < 10 ? "0" : "") +
-				String(seconds);
-			setTime(timeString);
-			if (seconds > 0) {
-				timeoutHandle = setTimeout(tick, 1000);
-			} else {
-				if (minutes > 1) {
-					timeoutHandle = setTimeout(function () {
-						countDownMinutes(minutes - 1);
-					}, 1000);
-				}
-			}
-		};
 
-		tick();
-		console.log(timeoutHandle);
-	};*/
+	useEffect(() => {
+		let interval = null;
+		if (isActive && seconds >= 0) {
+			interval = setInterval(() => {
+				setSeconds((seconds) => seconds - 1);
+				let minutes = Math.floor(seconds / 60);
+				let countSec = Math.round((seconds / 60 - minutes) * 60);
+				setTime(
+					(minutes < 10 ? "0" : "") +
+						minutes +
+						":" +
+						(countSec < 10 ? "0" : "") +
+						countSec
+				);
+			}, 100);
+		} else if (!isActive && seconds < 0) {
+			clearInterval(interval);
+		}
+		return () => clearInterval(interval);
+	}, [isActive, seconds]);
+
 	return (
 		<>
 			<div className="init">
@@ -92,20 +68,10 @@ function App() {
 			<div className="process">
 				<div id="timer-label">Session</div>
 				<div id="time-left">{timeLeft}</div>
-				<button id="start_stop" onClick={ssss}>
-					{textButton}
+				<button id="start_stop" onClick={toggle}>
+					{isActive ? "Pause" : "Start"}
 				</button>
-				<button
-					id="reset"
-					onClick={() => {
-						setWork(25);
-						setRelax(5);
-						/*console.log("1", timeoutHandle);
-						clearTimeout(timeoutHandle);
-						console.log("2", timeoutHandle); //setTime(work + ":00");*/
-						setText("Start");
-					}}
-				>
+				<button id="reset" onClick={reset}>
 					Reset
 				</button>
 				<div></div>
